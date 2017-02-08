63f53e85c474743533c8a74d90ee5d801ae13156
lyy <liangyunyu@gmail.com>
2017-02-08 14:28:20 +0800

    All add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  #resources :topics
+  root 'topics#index'
+
+  resources :topics do
+    member do
+      post 'upvote'
+      #post :upvote
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
